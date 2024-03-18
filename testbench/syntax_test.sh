43b5a9405b43359bab964b680e7b027af63fe74f
ShengZixuan <chengzixuan21a@mails.ucas.ac.cn>
2024-03-18 18:10:33 +0800

    systax_test

--- testbench/syntax_test.sh
+++ testbench/syntax_test.sh
@@ -9,8 +9,8 @@ cact_dir="$prj_dir/test/samples_lex_and_syntax"
 for file in "$cact_dir"/*.cact; do
     if [[ -f "$file" ]]; then
         filename=$(basename "$file")
-        ans=$(echo "$filename" | grep -E "[0-9][0-9]_(true|false)" -o | grep -E "true|false" -o)
-        out=$($compiler $file 2>/dev/nullptr | tail -n 1 | grep -E "true|false")
+        ans=$(echo "$filename" | grep -E "[0-9][0-9]_(true|false)" -o | grep -E "true|false" -o) #ans为true或false，对应cact文件名，金标准
+        out=$($compiler $file 2>/dev/nullptr | tail -n 1 | grep -E "true|false") #提取到最后一行命令输出中包含的false或true
         if [[ "$out" != "$ans" ]]; then
             echo syntax test failed at $filename
             echo ans is "$ans"
