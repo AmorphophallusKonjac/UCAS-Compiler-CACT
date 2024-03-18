d9dfac9bd41e5732c767c35f65938851033edcef
konjac <wangmingyu21a@mails.ucas.ac.cn>
2024-03-18 03:26:32 +0000

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,68 @@
 #include <iostream>
+#include <ostream>
+#include <type_traits>
 
 // #include "antlr4-runtime.h"
 #include "tree/ErrorNode.h"
 
-#include "HelloLexer.h"
-#include "HelloParser.h"
-#include "HelloVisitor.h"
+#include "CACTLexer.h"
+#include "CACTParser.h"
+#include "CACTVisitor.h"
+#include "tree/ParseTree.h"
 
 using namespace antlr4;
 
-class Analysis : public HelloVisitor {
-public:
-    std::any visitR(HelloParser::RContext *context) {
-        visitChildren( context );
+// class Analysis : public HelloVisitor {
+// public:
+//     std::any visitR(HelloParser::RContext *context) {
+//         visitChildren( context );
         
-        std::cout << "enter rule [r]!" << std::endl;
-        std::cout << "the ID is: " << context->ID()->getText().c_str() << std::endl;
-        return nullptr;
-    }
+//         std::cout << "enter rule [r]!" << std::endl;
+//         std::cout << context->ID().data() << std::endl;
+//         std::cout << "the ID is: " << context->ID(0)->getText() << std::endl;
+//         std::cout << "the ID is: " << context->ID(1)->getText() << std::endl;
+//         return nullptr;
+//     }
 
-    std::any visitErrorNode(tree::ErrorNode * node) override {
-        std::cout << "visit error node!" << std::endl;
-        return nullptr;
-    }
-};
+//     std::any visitErrorNode(tree::ErrorNode * node) override {
+//         std::cout << "visit error node!" << std::endl;
+//         return nullptr;
+//     }
+// };
 
 int main(int argc, const char* argv[]) {
-  std::ifstream stream;
-  stream.open("../test.hello");
+  	if (argc < 2) {
+   	 	std::cout << "Error! Missing source file" << std::endl;
+		return 0;
+	}
+	std::ifstream stream;
+	stream.open(argv[1]);
 
-  ANTLRInputStream   input(stream);
-  HelloLexer         lexer(&input);
-  CommonTokenStream  tokens(&lexer);
-  HelloParser        parser(&tokens);
+	ANTLRInputStream   input(stream);
+	CACTLexer         lexer(&input);
+	CommonTokenStream  tokens(&lexer);
+	CACTParser        parser(&tokens);
 
-  Analysis visitor;
-  visitor.visit( parser.r() );
+	tree::ParseTree *ctx = parser.compilationUnit();
 
-  return 0;
+	int flag = 1;
+
+	if (lexer.getNumberOfSyntaxErrors() > 0) {
+		std::cout << "lex error: " << lexer.getNumberOfSyntaxErrors() << std::endl;
+		flag = 0;
+	}
+
+	if (parser.getNumberOfSyntaxErrors() > 0) {
+		std::cout << "syntax error: " << parser.getNumberOfSyntaxErrors() << std::endl;
+		flag = 0;
+	}
+
+	if (flag)
+		std::cout << "true" << std::endl;
+	else
+		std::cout << "false" << std::endl;
+
+	// std::cout << ctx->toStringTree() << std::endl;
+
+	return 0;
 }
