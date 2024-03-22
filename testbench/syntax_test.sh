1cdbf0f9a1e037d3cca2401244933f510b257535
ShengZixuan <chengzixuan21a@mails.ucas.ac.cn>
2024-03-22 11:49:29 +0800

    error

--- testbench/syntax_test.sh
+++ testbench/syntax_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-prj_dir="/root/CACT" 
+prj_dir="/home/szx/Documents/cact" 
 
 compiler="$prj_dir/build/compiler"
 
@@ -10,7 +10,7 @@ for file in "$cact_dir"/*.cact; do
     if [[ -f "$file" ]]; then
         filename=$(basename "$file")
         ans=$(echo "$filename" | grep -E "[0-9][0-9]_(true|false)" -o | grep -E "true|false" -o) #ans为true或false，对应cact文件名，金标准
-        out=$($compiler $file 2>/dev/nullptr | tail -n 1 | grep -E "true|false") #提取到最后一行命令输出中包含的false或true
+        out=$($compiler $file 2>/dev/null | tail -n 1 | grep -E "true|false") #提取到最后一行命令输出中包含的false或true
         if [[ "$out" != "$ans" ]]; then
             echo syntax test failed at $filename
             echo ans is "$ans"
