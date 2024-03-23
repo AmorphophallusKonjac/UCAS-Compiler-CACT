f403d810adc2491c7f343a8860fba770bd01f1cf
compiler06 <compiler06@example.com>
2024-03-23 17:36:24 +0800

    Update syntax_test.sh

--- testbench/syntax_test.sh
+++ testbench/syntax_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-prj_dir="/home/szx/Documents/cact" 
+prj_dir="root/CACT" 
 
 compiler="$prj_dir/build/compiler"
 
@@ -10,7 +10,7 @@ for file in "$cact_dir"/*.cact; do
     if [[ -f "$file" ]]; then
         filename=$(basename "$file")
         ans=$(echo "$filename" | grep -E "[0-9][0-9]_(true|false)" -o | grep -E "true|false" -o)
-        out=$($compiler $file 2>/dev/null | tail -n 1 | grep -E "true|false")
+        out=$($compiler $file 2>/dev/nullptr | tail -n 1 | grep -E "true|false")
         if [[ "$out" != "$ans" ]]; then
             echo syntax test failed at $filename
             echo ans is "$ans"
