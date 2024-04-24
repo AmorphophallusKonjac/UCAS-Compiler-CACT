2f258f56122bbc0e3f7e7340ddf2ca1da8f5f5c4
konjac <wangmingyu21a@mails.ucas.ac.cn>
2024-04-24 13:25:17 +0800

    fix type

--- src/IR/iMemory.cpp
+++ src/IR/iMemory.cpp
@@ -3,8 +3,9 @@
 #include "IRConstant.h"
 #include "IRDerivedTypes.h"
 
-IRAllocationInst::IRAllocationInst(IRType *Ty, IRValue *ArraySize, unsigned int iTy, const std::string &Name, IRBasicBlock *parent)
-    : IRInstruction(new IRPointerType(Ty), iTy, Name, parent) {
+IRAllocationInst::IRAllocationInst(IRType *Ty, IRValue *ArraySize, unsigned int iTy, const std::string &Name,
+                                   IRBasicBlock *parent)
+        : IRInstruction(new IRPointerType(Ty), iTy, Name, parent) {
     if (!ArraySize) ArraySize = IRConstantInt::get(1);
     Operands.reserve(1);
     Operands.emplace_back(ArraySize, this);
@@ -20,42 +21,44 @@ const IRType *IRAllocationInst::getAllocatedType() const {
 
 
 IRLoadInst::IRLoadInst(IRValue *Ptr, const std::string &Name, IRBasicBlock *parent)
-    : IRInstruction(dynamic_cast<IRPointerType *>(Ptr->getType())->getElementType(),
-                    Load, Name, parent),
-      Volatile(false) {
+        : IRInstruction(dynamic_cast<IRSequentialType *>(Ptr->getType())->getElementType(),
+                        Load, Name, parent),
+          Volatile(false) {
     Operands.reserve(1);
     Operands.emplace_back(Ptr, this);
 }
 
 IRLoadInst::IRLoadInst(IRValue *Ptr, const std::string &Name, bool isVolatile, IRBasicBlock *parent)
-    : IRInstruction(dynamic_cast<IRPointerType *>(Ptr->getType())->getElementType(),
-                    Load, Name, parent),
-      Volatile(isVolatile) {
+        : IRInstruction(dynamic_cast<IRSequentialType *>(Ptr->getType())->getElementType(),
+                        Load, Name, parent),
+          Volatile(isVolatile) {
     Operands.reserve(1);
     Operands.emplace_back(Ptr, this);
 }
 
 IRStoreInst::IRStoreInst(IRValue *Val, IRValue *Ptr, IRBasicBlock *parent)
-    : IRInstruction(IRType::VoidTy, Store, "", parent), Volatile(false) {
+        : IRInstruction(IRType::VoidTy, Store, "", parent), Volatile(false) {
     Operands.reserve(2);
     Operands.emplace_back(Val, this);
     Operands.emplace_back(Ptr, this);
 }
+
 IRStoreInst::IRStoreInst(IRValue *Val, IRValue *Ptr, bool isVolatile, IRBasicBlock *parent)
-    : IRInstruction(IRType::VoidTy, Store, "", parent), Volatile(isVolatile) {
+        : IRInstruction(IRType::VoidTy, Store, "", parent), Volatile(isVolatile) {
     Operands.reserve(2);
     Operands.emplace_back(Val, this);
     Operands.emplace_back(Ptr, this);
 }
 
 IRMemcpyInst::IRMemcpyInst(IRValue *SrcPtr, IRValue *DestPtr, IRBasicBlock *InsertBefore)
-    : IRInstruction(IRType::VoidTy, Memcpy, "", InsertBefore), Volatile(false) {
+        : IRInstruction(IRType::VoidTy, Memcpy, "", InsertBefore), Volatile(false) {
     Operands.reserve(2);
     Operands.emplace_back(SrcPtr, this);
     Operands.emplace_back(DestPtr, this);
 }
+
 IRMemcpyInst::IRMemcpyInst(IRValue *SrcPtr, IRValue *DestPtr, bool isVolatile, IRBasicBlock *parent)
-    : IRInstruction(IRType::VoidTy, Memcpy, "", parent), Volatile(isVolatile) {
+        : IRInstruction(IRType::VoidTy, Memcpy, "", parent), Volatile(isVolatile) {
     Operands.reserve(2);
     Operands.emplace_back(SrcPtr, this);
     Operands.emplace_back(DestPtr, this);
