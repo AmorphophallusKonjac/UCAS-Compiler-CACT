299b89a7a8df63a7a4822fb44609197f6c171cc9
konjac <wangmingyu21a@mails.ucas.ac.cn>
2024-05-14 20:29:57 +0800

    update: add EliminateBasicInductionVarPass

---
+++ src/Pass/EliminateBasicInductionVarPass.h
@@ -0,0 +1,14 @@
+#ifndef COMPILER_ELIMINATEBASICINDUCTIONVARPASS_H
+#define COMPILER_ELIMINATEBASICINDUCTIONVARPASS_H
+
+#include "Pass.h"
+
+class EliminateBasicInductionVarPass : public FunctionPass {
+public:
+    explicit EliminateBasicInductionVarPass(std::string name = "");
+
+    void runOnFunction(IRFunction &F) override;
+};
+
+
+#endif //COMPILER_ELIMINATEBASICINDUCTIONVARPASS_H
