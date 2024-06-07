d58d89f24b8c17492fa232d56087b3d68f4f3f0b
konjac <wangmingyu21a@mails.ucas.ac.cn>
2024-06-07 10:40:10 +0800

    update: return cact return val when simulate

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,7 @@ int main(int argc, const char *argv[]) {
         int ret = ip.interpret();
         if (parser.get("c"))
             printf("inst_cnt = %d\n", Interpreter::getInstCnt());
+        return ret;
     }
 
     return 0;
